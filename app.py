7b7db4e9617489f03e9ebfe00bc6988bf056af58
Yvette Nartey <newerkey@gmail.com>
2021-11-23 15:25:47 +0100

    setup database instance

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template , url_for
 from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 app = Flask(__name__)
 
+# for app to recognize and connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ctdatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db= SQLAlchemy(app)
 
+#database instance
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False )
+    last_name = db.Column(db.String(100), nullable=False )
+    phone_number =  db.Column(db.Integer(), nullable=False)
+    date_joined= db.Column(db.DateTime, default=datetime.utcnow)
+    email =  db.Column(db.String(100), nullable=False )
+    group =  db.Column(db.String(100) )
+
+
+
+
+#url request path to homepage
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
