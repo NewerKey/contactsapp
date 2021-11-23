81341cb1e1eca582d784bf523156cb6b44227059
Yvette Nartey <newerkey@gmail.com>
2021-11-24 00:35:33 +0100

    update database

--- contactsapp/__init__.py
+++ contactsapp/__init__.py
@@ -9,3 +9,7 @@ from contactsapp import routes
 
 
 
+# for app to recognize and connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY']= 'd76fbc47c80d4076ed4d653015ea2458a2cc2d58b965ab90e743ae40dcfdb92'
