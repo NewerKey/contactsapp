0ad7fc6a2028973d065ad733856ca5ba8b4921ce
Yvette Nartey <newerkey@gmail.com>
2021-11-24 00:17:37 +0100

    build for production

--- contactsapp/__init__.py
+++ contactsapp/__init__.py
@@ -9,7 +9,6 @@ db= SQLAlchemy(app)
 # for app to recognize and connect to database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '13b7cf64836c54c780f2a097'
 
 
 from contactsapp import routes
