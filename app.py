58533ff5b810e76c8e7f1d7128002455b795c704
Yvette Nartey <newerkey@gmail.com>
2021-11-24 01:15:31 +0100

    changes to app

---
+++ app.py
@@ -0,0 +1,64 @@
+from sys import excepthook
+from flask import Flask,render_template , url_for , redirect , request , flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+db= SQLAlchemy(app)
+
+# for app to recognize and connect to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY']= 'd76fbc47c80d4076ed4d653015ea2458a2cc2d58b965ab90e743ae40dcfdb92'
+
+#database instance
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False )
+    email =  db.Column(db.String(100), nullable=False )
+    phonenumber =  db.Column(db.String(), nullable=False)
+    group =  db.Column(db.String(100))
+
+
+    def __init__(self, name, phonenumber, email, group):
+        self.name = name
+        self.email = email
+        self.phonenumber = phonenumber
+        self.group = group
+
+
+#url request path to homepage
+@app.route('/')
+def index():
+    contacts = Contact.query.all()
+    return render_template("index.html", contacts=contacts)
+
+
+#adding contact
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        name= request.form['name']
+        phonenumber= request.form['phone']
+        email= request.form['email']
+        group= request.form['group']
+
+        new_contact = Contact(name, email, phonenumber, group)
+        db.session.add(new_contact)
+        db.session.commit()
+        flash("Contact saved")
+        return redirect(url_for('index'))
+
+#deleting contact
+@app.route('/delete/<id>/', methods= ['GET', 'POST'])
+def delete(id):
+    contact_to_delete = Contact.query.get(id)
+    db.session.delete(contact_to_delete)
+    db.session.commit()
+    flash("Contact deleted")
+    
+    return redirect(url_for('index'))
+
+#checks if run.py file has executed directly and not imported
+if __name__== '__main__':
+    app.run(debug=True)
--- run.py
+++
@@ -1,6 +0,0 @@
-from contactsapp import app
-
-
-#checks if run.py file has executed directly and not imported
-if __name__== '__main__':
-    app.run(debug=True)
